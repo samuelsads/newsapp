ffc246614cf463291145a2b7b538cb06b46e55af
Samuel D <samueldzibsads@gmail.com>
2024-09-28 16:04:56 -0600

    Feature/general-configs add core/get_it/service_locator.dart

---
+++ lib/core/get_it/service_locator.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:get_it/get_it.dart';
+/// Global service locator instance for dependency injection.
+final serviceLocator  = GetIt.instance;
+
+/// Sets up the service locator for dependency injection.
+///
+/// This function initializes the global service locator instance and can be called
+/// to register dependencies before they are needed throughout the application.
+void setUpServiceLocator(){
+
+}
