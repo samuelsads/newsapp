dad7187968bf137ecab5af32bd9ae62491785f26
Samuel D <samueldzibsads@gmail.com>
2024-09-28 16:19:52 -0600

    Feature/general-configs add setUpServiceLocator to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:newsapp/core/get_it/service_locator.dart';
 import 'package:newsapp/news_app.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -14,7 +15,7 @@ void main() async {
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
 
+  setUpServiceLocator();
+
   runApp(const NewsApp());
 }
-
-
